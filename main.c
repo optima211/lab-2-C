c2220880dcee716417a51fa36eb5c1a8ecf4536a
optima211 <nedmk@mail.ru>
2018-12-03 15:07:32 +0700

    finish building first structure

--- main.c
+++ main.c
@@ -11,53 +11,57 @@ struct student {
 int main() {
     printf("Hello, World!\n");
     struct student st1;
-    st1.surname = 'I';
+    st1.surname = 'A';
     st1.year = 1998;
     st1.fac = 'p';
 
     struct student st2;
-    st2.surname = 'P';
+    st2.surname = 'B';
     st2.year = 1999;
     st2.fac = 'h';
 
     struct student st3;
-    st3.surname = 'S';
+    st3.surname = 'C';
     st3.year = 1980;
     st3.fac = 'p';
 
     struct student st4;
-    st4.surname = 'B';
+    st4.surname = 'D';
     st4.year = 1995;
     st4.fac = 'p';
 
     struct student st5;
-    st5.surname = 'S';
+    st5.surname = 'E';
     st5.year = 1993;
     st5.fac = 'h';
 
     struct student st6;
-    st6.surname = 'B';
+    st6.surname = 'F';
     st6.year = 1998;
     st6.fac = 'm';
 
     struct student st7;
-    st7.surname = 'O';
+    st7.surname = 'G';
     st7.year = 1997;
     st7.fac = 'p';
 
     struct student st8;
-    st8.surname = 'K';
+    st8.surname = 'H';
     st8.year = 1995;
     st8.fac = 'm';
 
     struct student st9;
-    st9.surname = 'G';
+    st9.surname = 'I';
     st9.year = 1993;
     st9.fac = 'h';
 
     struct student st10;
-    st10.surname = 'B';
+    st10.surname = 'G';
     st10.year = 1982;
     st10.fac = 'm';
+
+    printf("st1 faculty is %c\n", st1.surname);
+    printf("st1 surname is %d\n", st1.year);
+    printf("st1 faculty is %c\n", st1.fac);
     return 0;
 }
