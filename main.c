7cb5d82504683e35ac5f44a7e98cc1ad4bd89c63
optima211 <nedmk@mail.ru>
2018-12-03 14:31:50 +0700

    deleted error type 'char'

--- main.c
+++ main.c
@@ -1,61 +1,63 @@
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
+
 struct student {
     char surname;
     int year;
     char fac;
 };
+
 int main() {
     printf("Hello, World!\n");
     struct student st1;
-    st1.surname = 'Ivanov';
+    st1.surname = 'I';
     st1.year = 1998;
-    st1.fac = 'phusic';
+    st1.fac = 'p';
 
     struct student st2;
-    st2.surname = 'Petrov';
+    st2.surname = 'P';
     st2.year = 1999;
-    st2.fac = 'history';
+    st2.fac = 'h';
 
     struct student st3;
-    st3.surname = 'Sidorov';
+    st3.surname = 'S';
     st3.year = 1980;
-    st3.fac = 'phusic';
+    st3.fac = 'p';
 
     struct student st4;
-    st4.surname = 'Bashkov';
+    st4.surname = 'B';
     st4.year = 1995;
-    st4.fac = 'phusic';
+    st4.fac = 'p';
 
     struct student st5;
-    st5.surname = 'Skripchenko';
+    st5.surname = 'S';
     st5.year = 1993;
-    st5.fac = 'history';
+    st5.fac = 'h';
 
     struct student st6;
-    st6.surname = 'Baranov';
+    st6.surname = 'B';
     st6.year = 1998;
-    st6.fac = 'mathematic';
+    st6.fac = 'm';
 
     struct student st7;
-    st7.surname = 'Obolencev';
+    st7.surname = 'O';
     st7.year = 1997;
-    st7.fac = 'phusic';
+    st7.fac = 'p';
 
     struct student st8;
-    st8.surname = 'Kuznecov';
+    st8.surname = 'K';
     st8.year = 1995;
-    st8.fac = 'mathematic';
+    st8.fac = 'm';
 
     struct student st9;
-    st8.surname = 'Gusev';
-    st8.year = 1993;
-    st8.fac = 'history';
+    st9.surname = 'G';
+    st9.year = 1993;
+    st9.fac = 'h';
 
     struct student st10;
-    st8.surname = 'Bittirov';
-    st8.year = 1982;
-    st8.fac = 'mathematic';
+    st10.surname = 'B';
+    st10.year = 1982;
+    st10.fac = 'm';
     return 0;
 }
