6ac6ef8c99d2fa06b8e9b125dd0b9ac36200e507
optima211 <nedmk@mail.ru>
2018-12-03 23:33:24 +0700

    all if`s transform to 'switch-case'-construction

--- main.c
+++ main.c
@@ -62,23 +62,51 @@ int main() {
     st10.fac = 'm';
     printf("---------------------\n Convention:\n 1 - Physic\n 2 - History\n 3 - Mathematics\n---------------------\n Insert faculty:\n");
     scanf_s("%d", &x);
-    if (x == 1) {
-        printf("st1 faculty is %c\n", st1.surname);
-        printf("st1 surname is %d\n", st1.year);
-        printf("st1 faculty is %c\n", st1.fac);
-
-        printf("st3 faculty is %c\n", st3.surname);
-        printf("st3 surname is %d\n", st3.year);
-        printf("st3 faculty is %c\n", st3.fac);
-
-        printf("st4 faculty is %c\n", st4.surname);
-        printf("st4 surname is %d\n", st4.year);
-        printf("st4 faculty is %c\n", st4.fac);
-
-        printf("st7 faculty is %c\n", st7.surname);
-        printf("st7 surname is %d\n", st7.year);
-        printf("st7 faculty is %c\n", st7.fac);
-    } else { printf("finish!"); }
-    _getch();
+    switch (x) {
+        case 1:
+            printf("st1 surname is %c\n", st1.surname);
+            printf("st1 year of birth is %d\n", st1.year);
+            printf("st1 faculty is %c\n", st1.fac);
+            printf("---------------------\n");
+            printf("st3 surname is %c\n", st3.surname);
+            printf("st3 year of birth is %d\n", st3.year);
+            printf("st3 faculty is %c\n", st3.fac);
+            printf("---------------------\n");
+            printf("st4 surname is %c\n", st4.surname);
+            printf("st4 year of birth is %d\n", st4.year);
+            printf("st4 faculty is %c\n", st4.fac);
+            printf("---------------------\n");
+            printf("st7 surname is %c\n", st7.surname);
+            printf("st7 year of birth is %d\n", st7.year);
+            printf("st7 faculty is %c\n", st7.fac);
+            break;
+        case 2:
+            printf("st2 surname is %c\n", st2.surname);
+            printf("st2 year of birth is %d\n", st2.year);
+            printf("st2 faculty is %c\n", st2.fac);
+            printf("---------------------\n");
+            printf("st5 surname is %c\n", st5.surname);
+            printf("st5 year of birth is %d\n", st5.year);
+            printf("st5 faculty is %c\n", st5.fac);
+            printf("---------------------\n");
+            printf("st9 surname is %c\n", st9.surname);
+            printf("st9 year of birth is %d\n", st9.year);
+            printf("st9 faculty is %c\n", st9.fac);
+            break;
+        case 3:
+            printf("st6 surname is %c\n", st6.surname);
+            printf("st6 year of birth is %d\n", st6.year);
+            printf("st6 faculty is %c\n", st6.fac);
+            printf("---------------------\n");
+            printf("st8 surname is %c\n", st8.surname);
+            printf("st8 year of birth is %d\n", st8.year);
+            printf("st8 faculty is %c\n", st8.fac);
+            printf("---------------------\n");
+            printf("st10 surname is %c\n", st10.surname);
+            printf("st10 year of birth is %d\n", st10.year);
+            printf("st10 faculty is %c\n", st10.fac);
+            break;
+        default:  printf("There is no such value!\n");
+    }
     return 0;
 }
